822e17e0448f845f6733ecae0e5561853f712240
karol17 <karol.mazur17@hotmail.com>
2016-12-14 21:25:48 +0100

    Strona głowna

---
+++ SklepInternetowy/SklepInternetowy/Infrastructure/UrlHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SklepInternetowy.Infrastructure
+{
+    public static class UrlHelpers
+    {
+        public static string KategorieIkonySciezka(this UrlHelper helper,string nazwaIkonyKategorii)
+        {
+            var IkonyKategoriiFolder = AppConfig.IkonyKategoriiFolderWzgledny;
+            var sciezka = Path.Combine(IkonyKategoriiFolder, nazwaIkonyKategorii);
+            var SciezkaBezwzgledna = helper.Content(sciezka);
+            return SciezkaBezwzgledna;
+        }
+        public static string ObrazkiSciezka(this UrlHelper helper, string nazwaObrazka)
+        {
+            var ObrazkiFolder = AppConfig.ObrazkiFolderWzgledny;
+        var sciezka = Path.Combine(ObrazkiFolder, nazwaObrazka);
+        var SciezkaBezwzgledna = helper.Content(sciezka);
+            return SciezkaBezwzgledna;
+        }
+}
+}
