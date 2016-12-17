c0b137bfa47bbcced3d61d45ac4f7d56e01ac49b
karol17 <karol.mazur17@hotmail.com>
2016-12-17 23:19:05 +0100

    porawka strony głównej

--- SklepInternetowy/SklepInternetowy/DAL/KursyInitializer.cs
+++ SklepInternetowy/SklepInternetowy/DAL/KursyInitializer.cs
@@ -22,13 +22,13 @@ namespace SklepInternetowy.DAL
         {
             var kategorie = new List<Kategoria>
             {
-                new Kategoria(){KategoriaId=1,NazwaKategorii="asp",NazwaPlikuIkony="asp.png",OpisKategorii="opis asp net mvc"},
-                new Kategoria(){KategoriaId=2,NazwaKategorii="java",NazwaPlikuIkony="java.png",OpisKategorii="opis java"},
-                new Kategoria(){KategoriaId=3,NazwaKategorii="php",NazwaPlikuIkony="php.png",OpisKategorii="opis php"},
-                new Kategoria(){KategoriaId=4,NazwaKategorii="html",NazwaPlikuIkony="html.png",OpisKategorii="opis html"},
-                new Kategoria(){KategoriaId=5,NazwaKategorii="css",NazwaPlikuIkony="css.png",OpisKategorii="opis css"},
-                new Kategoria(){KategoriaId=6,NazwaKategorii="xml",NazwaPlikuIkony="xml.png",OpisKategorii="opis xml"},
-                new Kategoria(){KategoriaId=7,NazwaKategorii="c#",NazwaPlikuIkony="c#.png",OpisKategorii="opis c#"},
+                new Kategoria(){KategoriaId=1,NazwaKategorii="asp",NazwaPlikuIkony="punktor.jpg",OpisKategorii="opis asp net mvc"},
+                new Kategoria(){KategoriaId=2,NazwaKategorii="java",NazwaPlikuIkony="punktor.jpg",OpisKategorii="opis java"},
+                new Kategoria(){KategoriaId=3,NazwaKategorii="php",NazwaPlikuIkony="punktor.jpg",OpisKategorii="opis php"},
+                new Kategoria(){KategoriaId=4,NazwaKategorii="html",NazwaPlikuIkony="punktor.jpg",OpisKategorii="opis html"},
+                new Kategoria(){KategoriaId=5,NazwaKategorii="css",NazwaPlikuIkony="punktor.jpg",OpisKategorii="opis css"},
+                new Kategoria(){KategoriaId=6,NazwaKategorii="xml",NazwaPlikuIkony="punktor.jpg",OpisKategorii="opis xml"},
+                new Kategoria(){KategoriaId=7,NazwaKategorii="c#",NazwaPlikuIkony="punktor.jpg",OpisKategorii="opis c#"},
                 
            
             };
@@ -37,13 +37,13 @@ namespace SklepInternetowy.DAL
 
             var kursy = new List<Kurs>
             {
-                new Kurs(){AutorKusrsu="tomek",NazwaKursu="asp.net mvc",KategoriaId=1,CenaKursu=99,Bestseller=true,NazwaPlikuObrazka="asp.png",
+                new Kurs(){AutorKusrsu="tomek",NazwaKursu="asp.net mvc",KategoriaId=1,CenaKursu=99,Bestseller=true,NazwaPlikuObrazka="asp.jpg",
                 DataDodania=DateTime.Now,OpisKursu="opis 1"},
-                 new Kurs(){AutorKusrsu="jacek",NazwaKursu="asp.net mvc3",KategoriaId=1,CenaKursu=120,Bestseller=true,NazwaPlikuObrazka="aspmvc3.png",
+                 new Kurs(){AutorKusrsu="jacek",NazwaKursu="asp.net mvc3",KategoriaId=1,CenaKursu=120,Bestseller=true,NazwaPlikuObrazka="asp-net-mvc-3.gif",
                 DataDodania=DateTime.Now,OpisKursu="opis 3"},
-                 new Kurs(){AutorKusrsu="irek",NazwaKursu="asp.net mvc4",KategoriaId=1,CenaKursu=120,Bestseller=true,NazwaPlikuObrazka="aspmvc4.png",
+                 new Kurs(){AutorKusrsu="irek",NazwaKursu="asp.net mvc4",KategoriaId=1,CenaKursu=120,Bestseller=true,NazwaPlikuObrazka="aspmvc.jpg",
                 DataDodania=DateTime.Now,OpisKursu="opis 4"},
-                 new Kurs(){AutorKusrsu="romek",NazwaKursu="asp.net mvc5",KategoriaId=1,CenaKursu=140,Bestseller=true,NazwaPlikuObrazka="aspmvc5.png",
+                 new Kurs(){AutorKusrsu="romek",NazwaKursu="asp.net mvc5",KategoriaId=1,CenaKursu=140,Bestseller=true,NazwaPlikuObrazka="asp.png",
                 DataDodania=DateTime.Now,OpisKursu="opis 5"},
 
             };
